8b63f8d9aa736b5974a8f96ee59f85b99ebac00e
Kiran-Ghosh004 <kiranghosh520@gmail.com>
2025-10-13 17:59:11 +0530

    password encryption and compare

--- aa/app.js
+++ aa/app.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+
 const app = express();
 const port = 3000;
 
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.cookie("name","kiran");
-  res.send("done");
+// app.get('/', (req, res) => {
+//   res.cookie("name","kiran");
+//   res.send("done");
+// });
+
+app.get('/', async (req, res) => {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash("kiran", salt);
+    console.log(salt);
+    console.log(hash);
+    res.send("done");
 });
+app.get('/compare', async (req, res) => {   
+    const isMatch = await bcrypt.compare("kiran", "$2b$10$VIj7cAVd4XSC1r51VU1Fs.PHE3w.fACmqJ9zgWtGyGxeJmbebS0vq");
+    console.log(isMatch);
+    res.send("done");
+});
+
 app.get('/read', (req, res) => {
 //   console.log(req.cookies);
   res.send("read page");
