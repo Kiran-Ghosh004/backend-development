363765f5ee9caf314211fbeaaeb092b3f4d1d67e
Kiran-Ghosh004 <kiranghosh520@gmail.com>
2025-10-13 17:42:11 +0530

    learnt ot set cookies nd read cookies

--- aa/app.js
+++ aa/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
+
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.cookie("name","kiran");
   res.send("done");
+});
+app.get('/read', (req, res) => {
+//   console.log(req.cookies);
+  res.send("read page");
 });
 
 app.listen(port, () => {
