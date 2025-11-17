1a778a150849e294b81523014d181a472276b48e
Kiran-Ghosh004 <kiranghosh520@gmail.com>
2025-11-17 21:20:42 +0530

    learning data association

--- data_association/app.js
+++ data_association/app.js
@@ -1,9 +1,31 @@
 const express= require('express');
 const app=express();
+const userModel=require('./models/user');
+const postModel=require('./models/post');
+const post = require('./models/post');
 
 app.get('/',(req,res)=>{
     res.send('Hello World!');
 }  );   
+app.get('/create',async (req,res)=>{
+    let user= await userModel.create({
+        username:"kiran",
+        email:"kiranghosh@gmail.com",
+        age:22,
+    })
+    res.send(user);
+});   
+app.get('/post/create',async (req,res)=>{
+    let post= await postModel.create({
+        postData:"This is my first post",
+        user:"691b427372c9d2b972fa141b"
+
+    })
+    let user= await userModel.findOne({_id:"691b427372c9d2b972fa141b"})
+    user.posts.push(post._id);
+    await user.save();
+    res.send({post,user});
+});   
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
--- data_association/models/user.js
+++ data_association/models/user.js
@@ -6,7 +6,11 @@ const userSchema = new mongoose.Schema({
     username: String,
     email: String,
     age: Number,
-    posts:Array
+    posts:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'post'
+        
+    }]
 });
 
 module.exports=mongoose.model('User', userSchema);
